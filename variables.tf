3e7d857f42616dc894e938b98f9343a3aadf68dd
M. Kozlik <malkak@skyvar.co.il>
2025-05-18 16:08:01 +0300

    Add variables

--- variables.tf
+++ variables.tf
@@ -1,41 +1,50 @@
 variable "project_id" {
   type = string
+  default = "kpmg-plansbot"
 }
 
 variable "host_project_id" {
   type = string
+  default = "kpmg-plansbot"
 }
 
 variable "region" {
   type = string
+  default = "me-west1"
 }
 
 # Network
 
 variable "vpc_name" {
   type = string
+  default = "dev"
 }
 
 variable "subnetwork_names" {
   type = list(string)
+  default = [ "nec-plan-compute-front", "nec-plan-compute-back", "nec-plan-compute-preprocess" ]
 }
 
 # Firestore resource
 
 variable "firestore_database_name" {
   type = string
+  default = "firestore-plansbot"
 }
 
 variable "table_names" {
-    type = list(string)
+  type = list(string)
+  default = [ "TA","TB" ]
 }
 
 # Cloud storage resource
 
 variable "cloud_storage_name" {
   type = string
+  default = "csplans"
 }
 
 variable "log_bucket_name" {
   type = string
+  default = "lbpalns"
 }
