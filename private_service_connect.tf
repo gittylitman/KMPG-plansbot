52cca73f4aa663a0b73808411d75f0f47c2d699e
GitiL <GitiL@skyvar.co.il>
2025-05-19 12:26:16 +0300

    Add endpoint psc

--- private_service_connect.tf
+++ private_service_connect.tf
@@ -2,14 +2,14 @@ resource "google_compute_address" "psc_ip" {
   name         = "psc-ip"
   address_type = "INTERNAL"
   region       = "me-west1"
-  subnetwork   = google_compute_subnetwork.subnetwork_psc.id
+  subnetwork   = data.google_compute_subnetwork.subnetwork_psc.id
 }
 
 resource "google_compute_forwarding_rule" "psc_google_apis" {
   name                  = "pscpmo" 
   region                = "me-west1"
   network               = google_compute_network.vpc_network.id
-  subnetwork            = google_compute_subnetwork.subnetwork_psc.id
+  subnetwork            = data.google_compute_subnetwork.subnetwork_psc.id
   ip_address            = google_compute_address.psc_ip.id
   load_balancing_scheme = "INTERNAL"
   ports                 = ["443"]
