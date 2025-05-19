e791415110a443fd6db610c8e592a03e79aa3a4c
S. Goldshmid <shoshig@skyvar.co.il>
2025-05-19 09:44:01 +0300

    Delete variables

--- variables.tf
+++ variables.tf
@@ -1,52 +1,43 @@
 variable "project_id" {
     type = string
-    default = "kpmg-plansbot"
 }
 
 variable "host_project_id" {
     type = string
-    default = "kpmg-plansbot"
 }
 
 variable "region" {
     type = string
-    default = "me-west1"
 }
 
 variable "service_account_name"{
     type = string
-    default = "kpmg-plansblot"
 }
 
 # front cloud run
 
 variable "front_cloud_run_name"{
     type = string
-    default = "pmo-front-gcp-prod"
 }
 
 variable "front_container_image" {
     type = string
-    default = "us-docker.pkg.dev/cloudrun/container/hello"
 }
 
 # cloud run
 
-variable "cloud_run_name"{
+variable "cloud_run_names"{
     type = list(string)
-    default = ["pmo-chat-gcp-prod","pmo-preprocess-gcp-prod"]
 }
 
 variable "container_image" {
     type = list(string)
-    default = ["us-docker.pkg.dev/cloudrun/container/hello","us-docker.pkg.dev/cloudrun/container/hello"]
 }
 
 # Network resource
 
 variable "vpc_name" {
     type = string
-    default = "dev"
 }
 
 variable "subnetwork_names" {
@@ -58,23 +49,19 @@ variable "subnetwork_names" {
 
 variable "firestore_database_name" {
     type = string
-    default = "firestore-plansbot2"
 
 }
 
 variable "table_names" {
     type = list(string)
-    default = [ "TA1","TB1" ]
 }
 
 # Cloud storage resource
 
 variable "cloud_storage_name" {
     type = string
-    default = "csplans1"
 }
 
 variable "log_bucket_name" {
     type = string
-    default = "lbpalns1"
 }
