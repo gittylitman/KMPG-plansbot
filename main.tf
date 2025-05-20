31aa3093bcf857c5c2a7d100090e28cc034c7b21
GitiL <GitiL@skyvar.co.il>
2025-05-20 13:47:46 +0300

    Change the resource

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "google_project_service" "firestore" {
   disable_on_destroy = false 
 }
 
-resource "google_project_service" "firestore" {
+resource "google_project_service" "dns" {
   project = var.project_id
   service = "dns.googleapis.com"
   disable_on_destroy = false 
