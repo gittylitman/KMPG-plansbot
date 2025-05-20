084165ead1b74858b0e904cb4664cbad40db6c93
GitiL <GitiL@skyvar.co.il>
2025-05-20 13:39:54 +0300

    Change the resource

--- private_service_connect.tf
+++ private_service_connect.tf
@@ -14,13 +14,45 @@
 #   ip_address            = google_compute_address.psc_ip.id
 # }
 
+  
+resource "google_compute_global_address" "psc_internal_ip" {
+  project      = var.project_id
+  name         = "endpoint-ip" # שם לכתובת, למשל pscpmo-ip
+  address_type = "INTERNAL"
+  purpose      = "PRIVATE_SERVICE_CONNECT"
+  network      = data.google_compute_network.vpc_network.id
+  address      = "10.0.7.5" # הכתובת IP הספציפית שאת רוצה
+}
 
-resource "google_compute_forwarding_rule" "psc_google_apis" {
-  name                  = "psc-google-apis"
-  region                = var.region
+resource "google_compute_global_forwarding_rule" "psc_endpoint" {
+  project               = var.project_id
+  name                  = "pscpmo"
+  target                = "all-apis"
   network               = data.google_compute_network.vpc_network.id
-  subnetwork            = data.google_compute_subnetwork.subnetwork[2].name
-  load_balancing_scheme = "INTERNAL"
-  target                = "all-google-apis"
-  ports                 = ["443"]
+  ip_address            = google_compute_global_address.psc_internal_ip.self_link
+  load_balancing_scheme = "" 
+  ip_protocol           = "TCP" 
+}
+
+resource "google_dns_managed_zone" "private_googleapis_zone" {
+  project     = var.project_id
+  name        = "googleapis-com-private-zone" 
+  dns_name    = "googleapis.com."           
+  description = "Private DNS zone for googleapis.com to route via PSC"
+  visibility  = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = data.google_compute_network.vpc_network.id
+    }
+  }
+}
+
+resource "google_dns_record_set" "wildcard_googleapis_a_record" {
+  project      = var.project_id
+  managed_zone = google_dns_managed_zone.private_googleapis_zone.name
+  name         = "*.googleapis.com." 
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [google_compute_global_address.psc_internal_ip.address]
 }
