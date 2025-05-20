60b795ed0f0ddfdeaf3b3e91174621d88fcb14a5
GitiL <GitiL@skyvar.co.il>
2025-05-20 10:36:36 +0300

    Add psc

--- private_service_connect.tf
+++ private_service_connect.tf
@@ -1,17 +1,15 @@
 resource "google_compute_address" "psc_ip" {
   name         = "psc-ip"
-  address_type = "INTERNAL"
   region       = "me-west1"
   subnetwork   = data.google_compute_subnetwork.subnetwork[2].name
+  address_type = "INTERNAL"
 }
 
 resource "google_compute_forwarding_rule" "psc_google_apis" {
   name                  = "pscpmo" 
   region                = "me-west1"
-  network               = data.google_compute_network.vpc_network.id
-  subnetwork            = data.google_compute_subnetwork.subnetwork_psc.id
-  ip_address            = google_compute_address.psc_ip.id
-  load_balancing_scheme = "INTERNAL"
-  ports                 = ["443"]
+  load_balancing_scheme = ""
   target                = "all-apis"
+  network               = data.google_compute_network.vpc_network.id
+  ip_address            = google_compute_address.psc_ip.id
 }
